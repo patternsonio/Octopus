d13bd977f2d8a4a834541be7a40ffe150d49e01b
Lasse Diercks <github@lassediercks.de>
2018-06-25 14:04:50 +0200

    feat(Input): introduce onChange api

--- src/components/op-input/op-input.tsx
+++ src/components/op-input/op-input.tsx
@@ -7,13 +7,19 @@ import { Component, Prop } from '@stencil/core';
 export class OpInput {
   @Prop() type: string;
   @Prop() label: string;
+  @Prop() onChange: any;
   @Prop() id: string;
   render() {
     return (
       <div class="op-input__wrap">
         <op-label>
           <span class="op-input__text">{this.label}</span>
-          <input id={this.id || null} class="op-input" type={this.type} />
+          <input
+            onChange={this.onChange}
+            id={this.id || null}
+            class="op-input"
+            type={this.type}
+          />
         </op-label>
       </div>
     );
